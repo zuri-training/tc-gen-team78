1f8784646da896eb7e09efdba3060fcc6e9f8674
favour shaw <41517965+Favourshaw@users.noreply.github.com>
2022-08-10 11:30:06 +0100

    Add files via upload

---
+++ tcfinal/static/js/pdf.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 10,
+                filename: 'myfile.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
